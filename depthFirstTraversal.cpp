c3154478ddd9fe52dcf8a86d69064879674c26f3
EshanJayasundara <e19163@eng.pdn.ac.lk>
2024-01-13 23:47:41 +0530

    depth first traversal

--- depthFirstTraversal.cpp
+++ depthFirstTraversal.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int node[] = {2, 5, 7, 3, 5 ,2, 6 , NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
-// dfs in a binary tree
+// dft in a binary tree
 void dfs(int i) {
     if(node[i] == NULL) {
         return;
     }
-    cout << node[i] << " ";
+    cout << node[i] << " "; // pre-order
     dfs((i+1)*2-1);
+    cout << node[i] << " "; // in-order
     dfs((i+1)*2);
+    cout << node[i] << " "; // post-order
 }
 
 int main() {
