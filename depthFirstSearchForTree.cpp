80454229bd821273341a48cc873bc3ab1e21d204
EshanJayasundara <e19163@eng.pdn.ac.lk>
2024-01-14 18:55:33 +0530

    dfs for a tree

--- depthFirstSearchForTree.cpp
+++ depthFirstSearchForTree.cpp
@@ -14,28 +14,33 @@ Node* createNode(int index, int data);
 void addChildren(Node* parent, int index, int data);
 
 
-// depth first search algorithm for a binary tree -----------------
+// depth first search algorithm for a tree -----------------
 bool visited[15];
 
 void dfs(Node* node) {
+    if(!visited[node->index]) {
+        cout << node->data << " ";
+    }
     visited[node->index] = true;
-    cout << node->data << " ";
     for (Node* n : node->children) {
         if (!visited[n->index]) {
             dfs(n);
         }
     }
 }
-// ----------------------------------------------------------------
+// ---------------------------------------------------------
 
 int main() {
-    Node* root = createNode(0, 2);
-    addChildren(root, 1, 5);
-    addChildren(root, 2, 7);
-    addChildren(root->children[0], 3, 3);
-    addChildren(root->children[0], 4, 5);
-    addChildren(root->children[1], 5, 2);
-    addChildren(root->children[1], 6, 6);
+    Node* root = createNode(0, 19);
+    addChildren(root, 1, 18);
+    addChildren(root, 2, 17);
+    addChildren(root, 3, 16);
+    addChildren(root->children[0], 4, 15);
+    addChildren(root->children[0], 5, 14);
+    addChildren(root->children[1], 6, 13);
+    addChildren(root->children[1], 7, 12);
+    addChildren(root->children[2], 8, 11);
+    addChildren(root->children[2], 9, 10);
     
     dfs(root);
 
